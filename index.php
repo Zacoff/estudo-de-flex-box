0cb7a6b78228788ef394f55b3a285363604160dd
Zacoff <daniel.dfa81@gmail.com>
2021-04-12 16:42:30 -0300

    estudo de flex box

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;700&display=swap" rel="stylesheet">
+
+    
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
+    <title>Dashboard</title>
+</head>
+<body>
+    <div class="sidebar">
+        <div class="topo">
+            <h3>Danki.code</h3>
+        </div><!-- topo -->
+        <div class="menu">
+            <ul>
+                <li><i class="fas fa-angle-double-right fa"></i><a href=""> Home</a></li>
+                <li><i class="fas fa-angle-double-right fa"></i><a href=""> Home</a></li>
+                <li><i class="fas fa-angle-double-right fa"></i><a href=""> Home</a></li>
+                <li><i class="fas fa-angle-double-right fa"></i><a href=""> Home</a></li>
+            </ul>
+        </div><!-- menu -->
+    </div><!-- sidebar -->
+    <!-- FIM DA SIDEBAR -->
+    <div class="main-container">
+        <header>
+            
+            <div class="pesquisa-campo">
+                <div class="icone-pesquisa"><i class="fas fa-search"></i></div><!-- icone pesquisa -->
+                <form action="">
+                <input type="text" name="" id="">
+                </form>
+                
+            </div><!-- pesquisa campo -->
+            <div class="icone-alerta">
+                <p>2</p>
+                <i class="fas fa-bell"></i>
+            </div>
+        </header>
+        <div class="content-1">
+            <div class="one"></div>
+            <div class="two"></div>
+            <div class="two"></div>
+            <div class="one"></div>
+        </div><!-- content 1 -->
+        <div class="content-2">
+            
+        </div><!-- content 2 -->
+    </div>
+</body>
+</html>
